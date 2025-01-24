df855255ef4988bfc0243cbca7bdf6e7fd60e065
Deepanshu-three <deepanshu.main@gmail.com>
2025-01-24 23:25:44 +0530

    added show all question functionality

--- backend/grpcServer.js
+++ backend/grpcServer.js
@@ -1,7 +1,7 @@
 import grpc from "@grpc/grpc-js"
 import protoLoader from "@grpc/proto-loader"
 import { PROTO_FILE_PATH, PROTO_FILE_LOAD_OPTIONS, GRPC_HOST } from "./utils/config.js"
-import { searchQuestions } from "./service/questionService.js"
+import { searchQuestions, showAllQuestions } from "./service/questionService.js"
 
 
 const packageDefination = protoLoader.loadSync(PROTO_FILE_PATH, PROTO_FILE_LOAD_OPTIONS)
@@ -13,7 +13,8 @@ const questionServices = packages.com.dvs.QuestionSearchService.service;
 const server = new grpc.Server();
 
 server.addService(questionServices, {
-    SearchQuestions: searchQuestions
+    SearchQuestions: searchQuestions,
+    ShowAllQuestions: showAllQuestions
 })
 
 export default server;
--- backend/service/questionService.js
+++ backend/service/questionService.js
@@ -7,7 +7,7 @@ export const searchQuestions = async (call, callback) => {
     // Build the query object
     const searchQuery = {
       title: { $regex: query, $options: "i" },
-      ...(questionType && { type: questionType })
+      ...(questionType && { type: questionType }),
     };
 
     console.log("Search Query:", searchQuery);
@@ -26,20 +26,40 @@ export const searchQuestions = async (call, callback) => {
 
     // Create response
     const response = {
-      questions: questions.map(q => ({
+      questions: questions.map((q) => ({
         id: q._id.toString(),
         type: q.type,
         anagramType: q.anagramType,
         blocks: q.blocks,
-        title: q.title
+        title: q.title,
       })),
       totalResults,
-      totalPages
+      totalPages,
     };
 
     callback(null, response);
   } catch (error) {
     console.error("Error in searchQuestions:", error);
+    callback(error);
+  }
+};
+
+export const showAllQuestions = async (call, callback) => {
+  try {
+    const questions = await Quiz.find({});
+    const response = {
+      questions: questions.map((q) => ({
+        id: q._id.toString(),
+        type: q.type,
+        anagramType: q.anagramType,
+        blocks: q.blocks,
+        title: q.title,
+      })),
+    };
+
+    callback(null, response);
+  } catch (error) {
+    console.error("Error in showAllQuestions:", error);
     callback(error);
   }
 };
